2a469de1f0625a76e137cb819ded641bf18a9257
emersonpessoa <eps011108@gmail.com>
2025-07-23 13:07:42 -0300

    feat: Created TransactionRepository  :rocket:

---
+++ src/main/java/br/com/emersonpesoa/picpay_desafio_backend/transaction/TransactionRepository.java
@@ -0,0 +1,7 @@
+package br.com.emersonpesoa.picpay_desafio_backend.transaction;
+
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface TransactionRepository extends ListCrudRepository<Transaction, Long> {
+
+}
