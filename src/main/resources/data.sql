a757a317619a65d3c3451bb0b2fb4723688ddb2a
emersonpessoa <eps011108@gmail.com>
2025-07-23 12:55:51 -0300

    feat: Created script in data.qsl  :rocket:

---
+++ src/main/resources/data.sql
@@ -0,0 +1,47 @@
+DELETE FROM
+    TRANSACTIONS;
+
+DELETE FROM
+    WALLETS;
+
+INSERT INTO
+    WALLETS (
+        ID,
+        FULL_NAME,
+        CPF,
+        EMAIL,
+        "PASSWORD",
+        "TYPE",
+        BALANCE
+    )
+VALUES
+    (
+        1,
+        'Joao - User',
+        12345678901,
+        'joao@test.com',
+        '123456',
+        1,
+        1000.00
+    );
+
+INSERT INTO
+    WALLETS (
+        ID,
+        FULL_NAME,
+        CPF,
+        EMAIL,
+        "PASSWORD",
+        "TYPE",
+        BALANCE
+    )
+VALUES
+(
+        2,
+        'Maria - User',
+        98765432101,
+        'maria@test.com',
+        '123456',
+        2,
+        1000.00
+    );
