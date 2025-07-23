6b9cfd93a4fa846410514ed9cbcd6fb120878539
emersonpessoa <eps011108@gmail.com>
2025-07-23 18:39:43 -0300

    feat: Added InvalidTransactionException :rocket:

---
+++ src/main/java/br/com/emersonpesoa/picpay_desafio_backend/exception/InvalidTransactionException.java
@@ -0,0 +1,8 @@
+package br.com.emersonpesoa.picpay_desafio_backend.exception;
+
+public class InvalidTransactionException extends RuntimeException {
+    public InvalidTransactionException(String message) {
+        super(message);
+    }
+
+}
