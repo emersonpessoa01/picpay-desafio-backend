8c181d8315dbb1fdc601946c016dab14b93e9e0d
emersonpessoa <eps011108@gmail.com>
2025-07-23 18:03:06 -0300

    fix: Moved WalletRepository :rocket:

--- src/main/java/br/com/emersonpesoa/picpay_desafio_backend/wallet/WalletRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.emersonpesoa.picpay_desafio_backend.wallet;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface WalletRepository extends CrudRepository<Wallet, Long> {
-
-}
