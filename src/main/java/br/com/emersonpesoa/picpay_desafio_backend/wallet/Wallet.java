a288cf2f945e77e329ddc0d586f520b888ad15d7
emersonpessoa <eps011108@gmail.com>
2025-07-23 11:40:34 -0300

    feat: Added Wallet :rocket:

---
+++ src/main/java/br/com/emersonpesoa/picpay_desafio_backend/wallet/Wallet.java
@@ -0,0 +1,16 @@
+package br.com.emersonpesoa.picpay_desafio_backend.wallet;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Id;
+
+public record Wallet(
+        @Id Long id,
+        String fullName,
+        String cpf,
+        String email,
+        String password,
+        int type,
+        BigDecimal balance) {
+
+}
