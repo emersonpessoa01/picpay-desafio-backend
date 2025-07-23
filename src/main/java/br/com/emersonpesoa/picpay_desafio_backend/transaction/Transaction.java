bd851260fdbb07ecc7316abe2352450c44614fea
emersonpessoa <eps011108@gmail.com>
2025-07-23 11:34:15 -0300

    feat: Created Transaction :sparkles:

--- src/main/java/br/com/emersonpesoa/picpay_desafio_backend/transaction/Transaction.java
+++ src/main/java/br/com/emersonpesoa/picpay_desafio_backend/transaction/Transaction.java
@@ -3,14 +3,22 @@ package br.com.emersonpesoa.picpay_desafio_backend.transaction;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Id;
+import org.springframework.data.annotation.CreatedDate;
 
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table(name = "transactions")
 public record Transaction(
     @Id Long id,
     Long payer,
     Long payee,
     BigDecimal value,
-    LocalDateTime createAt
+    @CreatedDate LocalDateTime createAt
 ) {
+    public Transaction{
+        // Definir o valor com duas casas decimais
+        value = value.setScale(2);
+    }
 
 }
