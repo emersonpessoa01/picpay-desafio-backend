174174464d72cde02ab0c1ed583b91526aea33c1
emersonpessoa <eps011108@gmail.com>
2025-07-24 14:28:48 -0300

    feat: Created NotificationException :rocket:

---
+++ src/main/java/br/com/emersonpesoa/picpay_desafio_backend/notification/NotificationException.java
@@ -0,0 +1,8 @@
+package br.com.emersonpesoa.picpay_desafio_backend.notification;
+
+public class NotificationException extends RuntimeException {
+    public NotificationException(String message) {
+        super(message);
+    }
+
+}
