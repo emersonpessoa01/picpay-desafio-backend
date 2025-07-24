e95d3c5a5029740a9bfb6a7c1eb0147b487db2d7
emersonpessoa <eps011108@gmail.com>
2025-07-24 12:34:14 -0300

    fix: Moved to transaction  :wrench:

--- src/main/java/br/com/emersonpesoa/picpay_desafio_backend/wallet/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.emersonpesoa.picpay_desafio_backend.wallet;
-
-import org.springframework.data.repository.ListCrudRepository;
-
-import br.com.emersonpesoa.picpay_desafio_backend.transaction.Transaction;
-
-public interface TransactionRepository extends ListCrudRepository<Transaction, Long> {
-
-}
