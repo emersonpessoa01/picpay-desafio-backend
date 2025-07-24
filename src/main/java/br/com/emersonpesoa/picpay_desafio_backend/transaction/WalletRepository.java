711b2b6895691020dc611fc54ab569f6c08d6a64
emersonpessoa <eps011108@gmail.com>
2025-07-24 12:31:57 -0300

    fix: Moved to transaction :rocket:

--- src/main/java/br/com/emersonpesoa/picpay_desafio_backend/transaction/WalletRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.emersonpesoa.picpay_desafio_backend.transaction;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.emersonpesoa.picpay_desafio_backend.wallet.Wallet;
-
-public interface WalletRepository extends CrudRepository<Wallet, Long> {
-
-}
