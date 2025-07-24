defd835cbcaef9b0fb3ea794910c1655dcdb8d51
emersonpessoa <eps011108@gmail.com>
2025-07-24 14:27:48 -0300

    feat: Created NotificationService :rocket:

---
+++ src/main/java/br/com/emersonpesoa/picpay_desafio_backend/notification/NotificationService.java
@@ -0,0 +1,11 @@
+package br.com.emersonpesoa.picpay_desafio_backend.notification;
+
+import org.springframework.stereotype.Service;
+
+import br.com.emersonpesoa.picpay_desafio_backend.transaction.Transaction;
+
+@Service
+public class NotificationService {
+    public void notify(Transaction transaction) {
+    }
+}
