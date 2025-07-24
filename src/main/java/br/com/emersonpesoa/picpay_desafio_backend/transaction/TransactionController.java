61df5f5a7344bb07f7d5fba13e4a019b7941d888
emersonpessoa <eps011108@gmail.com>
2025-07-24 14:40:47 -0300

    feat: Created TransactionController :rocket:

---
+++ src/main/java/br/com/emersonpesoa/picpay_desafio_backend/transaction/TransactionController.java
@@ -0,0 +1,29 @@
+package br.com.emersonpesoa.picpay_desafio_backend.transaction;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+    @PostMapping
+    public Transaction createTransaction(@RequestBody Transaction transaction) {
+        return transactionService.createTransaction(transaction);
+    }
+    @GetMapping
+        public List<Transaction> list() {
+            return transactionService.list();
+        }
+        
+
+}
