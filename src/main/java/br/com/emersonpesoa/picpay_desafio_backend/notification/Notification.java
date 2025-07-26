6ea60794baf23157c004b1f734e2de518abe7833
emersonpessoa <eps011108@gmail.com>
2025-07-26 14:57:06 -0300

    fix: Replaced the record with a traditional class Notification :sparkler::

--- src/main/java/br/com/emersonpesoa/picpay_desafio_backend/notification/Notification.java
+++ src/main/java/br/com/emersonpesoa/picpay_desafio_backend/notification/Notification.java
@@ -1,6 +1,30 @@
 package br.com.emersonpesoa.picpay_desafio_backend.notification;
 
-public record Notification(
-                boolean message) {
+public class Notification {
 
+    private String message;
+
+    // Construtor padrão (obrigatório para desserialização Jackson)
+    public Notification() {
+    }
+
+    // Construtor com parâmetro
+    public Notification(String message) {
+        this.message = message;
+    }
+
+    // Getter
+    public String getMessage() {
+        return message;
+    }
+
+    // Setter
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    // Método auxiliar para interpretar se a mensagem é autorizada
+    public boolean isAuthorized() {
+        return "Autorizado".equalsIgnoreCase(message);
+    }
 }
