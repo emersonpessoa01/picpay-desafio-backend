be1fb72037cc988b5eb493d3aa0c455a392f225a
emersonpessoa <eps011108@gmail.com>
2025-07-26 14:58:07 -0300

    fix: Adjusted NotificationConsumer :wrench:

--- src/main/java/br/com/emersonpesoa/picpay_desafio_backend/notification/NotificationConsumer.java
+++ src/main/java/br/com/emersonpesoa/picpay_desafio_backend/notification/NotificationConsumer.java
@@ -1,13 +1,19 @@
 package br.com.emersonpesoa.picpay_desafio_backend.notification;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
+import br.com.emersonpesoa.picpay_desafio_backend.authorization.AuthorizerService;
 import br.com.emersonpesoa.picpay_desafio_backend.transaction.Transaction;
 
 @Service
 public class NotificationConsumer {
+      private static final Logger LOGGER = LoggerFactory
+             .getLogger(AuthorizerService.class);
+
    private RestClient restClient;
    
    public NotificationConsumer(RestClient.Builder builder) {
@@ -15,15 +21,19 @@ public class NotificationConsumer {
          .baseUrl("https://9c8a6d1be79b411c80487d318e3ad3c7.api.mockbin.io/")
          .build();
    }
-   @KafkaListener(topics = "transation-notification", groupId = "picpay-desafio-backend")
+   @SuppressWarnings("null")
+   @KafkaListener(topics = "transaction-notification", groupId = "picpay-desafio-backend")
    public void receiveNotification(Transaction transaction) {
+      LOGGER.info("Authorizing transaction: {}", transaction); 
+
       var response = restClient.get()
          .retrieve()
          .toEntity(Notification.class);
         
-         if(response.getStatusCode().isError() || !response.getBody().message()) {
+         if(response.getStatusCode().isError() || response.getBody().getMessage() == null || response.getBody().getMessage().isEmpty()) {
             throw new NotificationException("Erro ao enviar notificação");
          }
+         LOGGER.info("Notification sent successfully for transaction: {}", response.getBody().getMessage());
          
 
    }
