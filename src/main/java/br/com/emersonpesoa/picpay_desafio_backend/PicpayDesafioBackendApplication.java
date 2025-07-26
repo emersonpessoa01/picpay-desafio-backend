46e785343f59d3b2ae3509290919bc4d6390389c
emersonpessoa <eps011108@gmail.com>
2025-07-26 14:54:19 -0300

    feat: Added method notificationTopic :sparkles:

--- src/main/java/br/com/emersonpesoa/picpay_desafio_backend/PicpayDesafioBackendApplication.java
+++ src/main/java/br/com/emersonpesoa/picpay_desafio_backend/PicpayDesafioBackendApplication.java
@@ -1,9 +1,12 @@
 package br.com.emersonpesoa.picpay_desafio_backend;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 @EntityScan(basePackages = {
@@ -14,5 +17,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class PicpayDesafioBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(PicpayDesafioBackendApplication.class, args);
+    }
+    @Bean
+    NewTopic notificationTopic(){
+        return TopicBuilder.name("transation-notification")
+                .build();
     }
 }
