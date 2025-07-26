b60146f5fc14760b24d3b87786cf7d84c3563ccd
emersonpessoa <eps011108@gmail.com>
2025-07-26 11:56:07 -0300

    fix: Adjusted PicpayDesafioBackendApplication :wrench:

--- src/main/java/br/com/emersonpesoa/picpay_desafio_backend/PicpayDesafioBackendApplication.java
+++ src/main/java/br/com/emersonpesoa/picpay_desafio_backend/PicpayDesafioBackendApplication.java
@@ -2,14 +2,17 @@ package br.com.emersonpesoa.picpay_desafio_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
+@EntityScan(basePackages = {
+        "br.com.emersonpesoa.picpay_desafio_backend.wallet",
+        "br.com.emersonpesoa.picpay_desafio_backend.transaction"
+})
+@EnableJpaAuditing
 public class PicpayDesafioBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PicpayDesafioBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PicpayDesafioBackendApplication.class, args);
+    }
 }
