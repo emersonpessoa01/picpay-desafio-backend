3f2c224ad2e1c6fa9dc7a711fbb279668fa8dc46
emersonpessoa <eps011108@gmail.com>
2025-07-26 15:00:05 -0300

    feat: Updated NotificationService :sparkles:

--- src/main/java/br/com/emersonpesoa/picpay_desafio_backend/notification/NotificationService.java
+++ src/main/java/br/com/emersonpesoa/picpay_desafio_backend/notification/NotificationService.java
@@ -6,6 +6,14 @@ import br.com.emersonpesoa.picpay_desafio_backend.transaction.Transaction;
 
 @Service
 public class NotificationService {
+    private final NotificationConsumer notificationConsumer;
+
+    public NotificationService(NotificationConsumer notificationConsumer) {
+        this.notificationConsumer = notificationConsumer;
+    }
     public void notify(Transaction transaction) {
+        // Envia a notificação para o consumidor Kafka
+        notificationConsumer.receiveNotification(transaction);
+        // Aqui você pode adicionar lógica adicional se necessário, como logging ou manipulação de erros
     }
 }
